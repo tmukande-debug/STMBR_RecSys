f8525f80deb31d02827cdb67ab74b97daa4d5071
Tendai Mukande <75079602+tmukande-debug@users.noreply.github.com>
2022-12-21 00:27:53 +0200

    Update configurator.py

--- recbole/config/configurator.py
+++ recbole/config/configurator.py
@@ -271,9 +271,9 @@ class Config(object):
             self.final_config_dict['data_path'] = os.path.join(current_path, '../dataset_example/' + self.dataset)
         else:
             #self.final_config_dict['data_path'] = os.path.join(self.final_config_dict['data_path'], self.dataset)
-            #self.final_config_dict['data_path'] = '/content/drive/MyDrive/RESEARCH_PROJECT/datasets/MBHT_dataset/retail_beh/'
+            self.final_config_dict['data_path'] = '/content/drive/MyDrive/RESEARCH_PROJECT/datasets/MBHT_dataset/retail_beh/'
             #self.final_config_dict['data_path'] = '/content/drive/MyDrive/RESEARCH_PROJECT/datasets/MBHT_dataset/ijcai_beh/'
-            self.final_config_dict['data_path'] = '/content/drive/MyDrive/RESEARCH_PROJECT/datasets/MBHT_dataset/tmall_beh/'
+            #self.final_config_dict['data_path'] = '/content/drive/MyDrive/RESEARCH_PROJECT/datasets/MBHT_dataset/tmall_beh/'
 
         if hasattr(self.model_class, 'input_type'):
             self.final_config_dict['MODEL_INPUT_TYPE'] = self.model_class.input_type
