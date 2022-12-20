5fed4612811f0aa0ac9017728f426d455f026779
Tendai Mukande <75079602+tmukande-debug@users.noreply.github.com>
2022-12-20 11:43:35 +0200

    Update mbht.py

--- recbole/model/sequential_recommender/mbht.py
+++ recbole/model/sequential_recommender/mbht.py
@@ -16,50 +16,6 @@ from recbole.model.layers import TransformerEncoder, HGNN
 #SOFT , rela-transformer, Nystromformer, conformer, compressive-transformer-pytorch,sinkhorn-transformer, GraphiT,flash_pytorchm Graph_Transformer_Networks, memory-efficient-attention-pytorch
 
 
-class BERT(pl.LightningModule):
-    def __init__(self,
-        max_len: int = None,
-        num_items: int = None,
-        n_layer: int = None,
-        n_head: int = None,
-        n_b: int = None,
-        d_model: int = None,
-        dropout: float = .0,
-        battn: bool = None,
-        bpff: bool = None,
-        brpb: bool = None, 
-    ):
-        super().__init__()
-        self.d_model = d_model
-        self.num_items = num_items
-        self.n_b = n_b
-        self.battn = battn
-        self.bpff = bpff
-        self.brpb = brpb
-        
-        vocab_size = num_items + 1 + n_b # add padding and mask 
-        # if self.brpb:
-        if True:
-            # simple embedding, adding behavioral relative positional bias in transformer blocks
-            self.embedding = SimpleEmbedding(vocab_size=vocab_size, embed_size=d_model, dropout=dropout)
-        else:
-            # embedding for BERT, sum of positional, token embeddings
-            self.embedding = BERTEmbedding(vocab_size=vocab_size, embed_size=d_model, max_len=max_len, dropout=dropout)
-        # multi-layers transformer blocks
-        self.transformer_blocks = nn.ModuleList(
-            [TransformerBlock(d_model, n_head, d_model * 4, n_b, battn, bpff, brpb, dropout) for _ in range(n_layer)])
-
-    def forward(self, x, b_seq):
-        # get padding masks
-        mask = (x > 0)
-        # embedding the indexed sequence to sequence of vectors
-        x = self.embedding(x)
-        # running over multiple transformer blocks
-        for transformer in self.transformer_blocks:
-            x = transformer.forward(x, b_seq, mask)
-        return x
-
-
 def sim(z1: torch.Tensor, z2: torch.Tensor):
     z1 = F.normalize(z1)
     z2 = F.normalize(z2)
@@ -354,16 +310,11 @@ class MBHT(SequentialRecommender):
         For memory, we only need calculate loss for masked position.
         Generate a multi-hot vector to indicate the masked position for masked sequence, and then is used for
         gathering the masked position hidden representation.
-
         Examples:
             sequence: [1 2 3 4 5]
-
             masked_sequence: [1 mask 3 mask 5]
-
             masked_index: [1, 3]
-
             max_length: 5
-
             multi_hot_embed: [[0 1 0 0 0], [0 0 0 1 0]]
         """
         masked_index = masked_index.view(-1)
