32ee2a0aaf886250115b65f60fb14351a2d34468
Tendai Mukande <75079602+tmukande-debug@users.noreply.github.com>
2023-02-27 12:12:59 +0000

    Update run BlcBert.py

--- run BlcBert.py
+++ run BlcBert.py
@@ -8,7 +8,7 @@ from recbole.data.utils import get_dataloader, create_samplers
 from recbole.utils import init_logger, init_seed, get_model, get_trainer, set_color
 #from recbole.model.sequential_recommender.blorectrans import MBHT
 #from recbole.model.sequential_recommender.mega import MBHT
-from recbole.model.sequential_recommender.nystrom import MBHT
+from recbole.model.sequential_recommender.blcbert import BERT4Rec
 
 def get_args():
     parser = argparse.ArgumentParser()
